e84bd398d15ad8b0ef4fcedc7f525fef2a8927f7
sabdulmajid <ayman.hasib@outlook.com>
2023-05-14 22:34:03 +0300

    Socket creation

---
+++ src/server.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define MAX_CONNECTIONS 10
+
+int main() {
+    // Create a socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        std::cerr << "Failed to create socket" << std::endl;
+        return 1;
+    }
+    
