08be149cb668388578a3bde6da05c9390fa8e957
sabdulmajid <ayman.hasib@outlook.com>
2023-05-21 13:08:25 +0300

    completed login function

--- src/authentication.cpp
+++ src/authentication.cpp
@@ -21,3 +21,33 @@ bool login(string username, string password, string twoFactorCode) {
         return false;
     }
 
+    rc = sqlite3_bind_text(stmt, 1, username.c_str(), -1, SQLITE_TRANSIENT);
+    if (rc != SQLITE_OK) {
+        cout << "Error binding username: " << sqlite3_errmsg(db) << endl;
+        return false;
+    }
+
+    rc = sqlite3_bind_text(stmt, 2, password.c_str(), -1, SQLITE_TRANSIENT);
+    if (rc != SQLITE_OK) {
+        cout << "Error binding password: " << sqlite3_errmsg(db) << endl;
+        return false;
+    }
+
+    rc = sqlite3_step(stmt);
+    if (rc != SQLITE_ROW) {
+        cout << "User not found" << endl;
+        return false;
+    }
+
+    // User found, check two-factor code
+    string storedTwoFactorCode = sqlite3_column_text(stmt, 3);
+    if (storedTwoFactorCode != twoFactorCode) {
+        cout << "Invalid two-factor code" << endl;
+        return false;
+    }
+
+    // User authenticated, return true
+    return true;
+}
+
+
