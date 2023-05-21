132ade6a14d4027201a6a3d1097d2cce9b7ec631
sabdulmajid <ayman.hasib@outlook.com>
2023-05-21 13:08:04 +0300

    login function, with username, password & 2FA

--- src/authentication.cpp
+++ src/authentication.cpp
@@ -12,3 +12,12 @@ public:
     string twoFactorCode;
 };
 
+bool login(string username, string password, string twoFactorCode) {
+    string query = "SELECT * FROM users WHERE username = ? AND password = ?";
+    sqlite3_stmt *stmt;
+    int rc = sqlite3_prepare(db, query.c_str(), -1, &stmt, NULL);
+    if (rc != SQLITE_OK) {
+        cout << "Error preparing statement: " << sqlite3_errmsg(db) << endl;
+        return false;
+    }
+
