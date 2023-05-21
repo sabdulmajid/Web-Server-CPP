308f9e9bf4e9c03080d4692ab2f5f9e1da5a8ff5
sabdulmajid <ayman.hasib@outlook.com>
2023-05-21 13:10:20 +0300

    Finalized access to web server

--- src/authentication.cpp
+++ src/authentication.cpp
@@ -50,4 +50,42 @@ bool login(string username, string password, string twoFactorCode) {
     return true;
 }
 
+int main() {
+    // Create a database to store user credentials.
+    sqlite3 *db;
+    int rc = sqlite3_open("database.db", &db);
+    if (rc != SQLITE_OK) {
+        cout << "Error opening database: " << sqlite3_errmsg(db) << endl;
+        return 1;
+    }
 
+    // Create a user class to represent a user in your application.
+    User user;
+    user.username = "username";
+    user.password = "password";
+    user.twoFactorCode = "twoFactorCode";
+
+    // Create a login function that checks the user credentials against the database.
+    bool isAuthenticated = login(user.username, user.password, user.twoFactorCode);
+    if (!isAuthenticated) {
+        cout << "Invalid username or password" << endl;
+        return 1;
+    }
+
+    // User logged in, welcome them to the application.
+    cout << "Welcome to the application, " << user.username << "!" << endl;
+
+    // Close the database.
+    sqlite3_close(db);
+
+    // Check if the user is authenticated.
+    if (isAuthenticated) {
+        // User is authenticated, allow them to access the web server.
+        cout << "You are now authenticated and have access to the web server." << endl;
+    } else {
+        // User is not authenticated, deny them access to the web server.
+        cout << "You are not authenticated and do not have access to the web server." << endl;
+    }
+
+    return 0;
+}
