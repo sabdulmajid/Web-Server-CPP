7cc19ad1809dc9f0ed498a3be8816f0200e8d3cb
sabdulmajid <ayman.hasib@outlook.com>
2023-05-21 12:56:34 +0300

    added FTP and SMTP support

--- src/server.cc
+++ src/server.cc
@@ -7,91 +7,122 @@
 
 #define MAX_CONNECTIONS 10
 
-int main() {
+int main()
+{
     // Create a socket
     int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == -1) {
+    if (server_fd == -1)
+    {
         std::cerr << "Failed to create socket" << std::endl;
         return 1;
     }
-    
+
     // Bind the socket to an address and port
     sockaddr_in address;
     std::memset(&address, 0, sizeof(address));
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
-    if (bind(server_fd, (sockaddr*)&address, sizeof(address)) == -1) {
+    if (bind(server_fd, (sockaddr *)&address, sizeof(address)) == -1)
+    {
         std::cerr << "Failed to bind socket to address" << std::endl;
         return 1;
     }
-    
+
     // Listen for incoming connections
-    if (listen(server_fd, MAX_CONNECTIONS) == -1) {
+    if (listen(server_fd, MAX_CONNECTIONS) == -1)
+    {
         std::cerr << "Failed to listen for connections" << std::endl;
         return 1;
     }
-    
+
     // Create a queue to store the connections
     std::queue<int> connections;
-    
-    while (true) {
+
+    while (true)
+    {
         // Accept an incoming connection
         sockaddr_in client_address;
         socklen_t client_address_len = sizeof(client_address);
-        int client_fd = accept(server_fd, (sockaddr*)&client_address, &client_address_len);
-        if (client_fd == -1) {
+        int client_fd = accept(server_fd, (sockaddr *)&client_address, &client_address_len);
+        if (client_fd == -1)
+        {
             std::cerr << "Failed to accept connection" << std::endl;
             continue;
         }
-        
+
         // Add the connection to the queue
         connections.push(client_fd);
-        
+
         // If the queue is full, handle the next connection in the queue
-        if (connections.size() == MAX_CONNECTIONS) {
+        if (connections.size() == MAX_CONNECTIONS)
+        {
             int next_connection = connections.front();
             connections.pop();
-            
+
             // Read the incoming request
             char buffer[1024];
             int num_bytes = read(next_connection, buffer, sizeof(buffer));
-            if (num_bytes == -1) {
+            if (num_bytes == -1)
+            {
                 std::cerr << "Failed to read request" << std::endl;
                 close(next_connection);
                 continue;
             }
-            
+
             // Parse the request
             std::string request(buffer, num_bytes);
             std::string protocol = request.substr(0, request.find(":"));
-            
+
             // Send the appropriate response
-            if (protocol == "FTP") {
-                // TODO: Implement FTP support
-            } else if (protocol == "SMTP") {
-                // TODO: Implement SMTP support
-            } else if (protocol == "HTTP") {
+            if (protocol == "FTP")
+            {
+                // Implement FTP support
+                std::cout << "FTP request received" << std::endl;
+                // Send the FTP response
+                std::string response =
+                    "220 Welcome to my FTP server\r\n";
+                num_bytes = write(next_connection, response.c_str(), response.size());
+            }
+            else if (protocol == "SMTP")
+            {
+                // Implement SMTP support
+                std::cout << "SMTP request received" << std::endl;
+                // Send the SMTP response
+                std::string response =
+                    "220 Welcome to my SMTP server\r\n";
+                num_bytes = write(next_connection, response.c_str(), response.size());
+            }
+            else if (protocol == "HTTP")
+            {
+                std::cout << "HTTP request received" << std::endl;
+                // Send the HTTP response
                 std::string response_body = "<html><body><h1>Hello, World!</h1></body></html>";
                 std::string response =
                     "HTTP/1.1 200 OK\r\n"
                     "Content-Type: text/html\r\n"
-                    "Content-Length: " + std::to_string(response_body.size()) + "\r\n"
-                    "\r\n" + response_body;
+                    "Content-Length: " +
+                    std::to_string(response_body.size()) + "\r\n"
+                                                           "\r\n" +
+                    response_body;
                 num_bytes = write(next_connection, response.c_str(), response.size());
-            } else if (protocol == "HTTPS") {
+            }
+            else if (protocol == "HTTPS")
+            {
                 // TODO: Implement HTTPS support
-            } else {
+            }
+            else
+            {
                 std::cerr << "Unsupported protocol: " << protocol << std::endl;
             }
-            
+
             // Close the connection
             close(next_connection);
         }
     }
-    
+
     // Close the server socket
     close(server_fd);
-    
+
     return 0;
 }
